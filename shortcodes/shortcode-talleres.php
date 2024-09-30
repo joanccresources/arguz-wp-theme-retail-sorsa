5ebf22d124fce5546f3f448304ef028ac9aaae26
Joan C <joan0492@gmail.com>
2024-09-30 01:04:04 -0500

    se maqueto la pagina de talleres

--- shortcodes/shortcode-talleres.php
+++ shortcodes/shortcode-talleres.php
@@ -1,4 +1,5 @@
 <?php
+/*
 function shortcode_talleres_sorsa_cards($atts)
 {
   // Inicia la salida del buffer
@@ -135,6 +136,110 @@ function shortcode_talleres_sorsa_cards($atts)
   </div> <!-- .elementor-element -->
 <?php
   // Retorna la salida
+  return ob_get_clean();
+}
+add_shortcode("talleres_sorsa_cards", "shortcode_talleres_sorsa_cards");
+*/
+
+
+function shortcode_talleres_sorsa_cards($atts)
+{
+  // Inicia la salida del buffer
+  ob_start();
+  $atts = shortcode_atts(array(
+    'post' => '', // Atributo opcional para el código del post
+    'title' => '',
+  ), $atts, 'talleres_sorsa_cards');
+?>
+
+
+  <div class="card-tienda__container">
+    <?php
+    // Consulta personalizada para obtener los posts del custom post type "punto-de-venta"
+    $args = array(
+      'post_type' => 'punto_de_venta',
+      'posts_per_page' => -1
+    );
+
+    // Si se proporciona un código de post, agregar la condición de post__in
+    if (!empty($atts['post'])) {
+      // $args['post__in'] = array($atts['post']);
+
+      // Convertir los códigos de post a un array
+      $post_ids = array_map('trim', explode(',', $atts['post']));
+      $post_ids = array_filter($post_ids, 'is_numeric'); // Asegurarse de que solo contenga valores numéricos
+      $args['post__in'] = $post_ids;
+      $args['orderby'] = 'post__in'; // Mantener el orden de los IDs especificados
+    }
+
+    $query = new WP_Query($args);
+
+    // Verifica si hay posts
+    if ($query->have_posts()):
+      while ($query->have_posts()):
+        $query->the_post();
+
+        $post_id = get_the_ID();
+
+        $title = get_the_title();
+
+        if (!empty($atts['title'])) {
+          // Procesar el título para quitar la primera parte ("Agencia") y agregar el prefijo
+          $title = trim($title); // Eliminar espacios alrededor
+          $post_title_parts = explode(' ', $title, 2); // Dividir en dos partes
+          if (count($post_title_parts) > 1) {
+            $title = wp_kses_post('<span class="text-azul-sorsa">' . $atts['title']) . '</span> ' . $post_title_parts[1]; // Eliminar la primera parte y agregar el prefijo
+          } else {
+            $title = wp_kses_post('<span class="text-azul-sorsa">' . $atts['title']) . '</span> ' . $title; // Si el título solo tiene una parte
+          }
+        } else {
+          // $title = esc_html($title); // Si no se proporciona un título, usar el título original
+          $words = explode(' ', $title, 2);
+          $first_word = $words[0];
+          $rest = $words[1] ?? '';
+          $title = wp_kses_post('<span class="text-azul-sorsa">' . $first_word . '</span>' . ' ' . $rest); // Si el título solo tiene una parte
+        }
+
+        // Obtiene los datos necesarios
+        $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); // Imagen destacada        
+        $content = get_the_content(); // Contenido del post
+
+        $url_mapa = get_post_meta($post_id, 'url_mapa', true); // Campo personalizado "url_mapa"
+
+        // Genera el HTML con los datos obtenidos
+    ?>
+        <div class="card-tienda wow fadeIn slow" data-wow-delay="0.2s">
+          <div class="row justify-content-between align-items-center">
+            <div class="col-lg-6">
+              <a
+                target="_blank"
+                href="<?= esc_url($url_mapa); ?>"
+                class="card-tienda__figure">
+                <img decoding="async" src="<?= esc_url($featured_img_url); ?>" alt="" class="card-tienda__img" />
+              </a>
+            </div>
+            <div class="col-lg-6">
+              <div class="card-tienda__info">
+                <h6 class="card-tienda__title">
+                  <a href="<?= esc_url($url_mapa); ?>" target="_blank"><?= wp_kses_post($title); ?></a>
+                </h6>
+                <div class="card-tienda__description">
+                  <?= wp_kses_post($content); ?>
+                </div>
+                <div class="btn-amarillo-blanco">
+                  <a href="<?= esc_url($url_mapa); ?>" target="_blank">VER EN EL MAPA</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    <?php
+      endwhile;
+      wp_reset_postdata();
+    endif;
+    ?>
+  </div>
+<?php
   return ob_get_clean();
 }
 add_shortcode("talleres_sorsa_cards", "shortcode_talleres_sorsa_cards");
